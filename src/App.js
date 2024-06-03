47af3d232aa3208965d9b9c79bfce40965e08dd5
Alina Korsukova <korsualin.work@gmail.com>
2024-06-03 20:53:03 +0300

    adding cart

--- src/App.js
+++ src/App.js
@@ -2,6 +2,49 @@
 function App() {
   return (
       <div className="wrapper">
+        <div className="overlay">
+          <div className="cartContainer">
+            <span className="cartTitle">Корзина</span>
+            <ul>
+              <li className="cartItem">
+                <img src="/img/sneakers/sneakers1mini.png" className="cartImg" alt="sneakers Puma X" />
+                <div className="descriptionItem">
+                  <p className="nameSneakers">Кроссовки Puma X Aka Boku Future Rider</p>
+                  <div className="price">
+                    <strong>8 999 руб.</strong>
+                    <img src="/img/deletecart.svg" className="deleteIcon" alt="delete cart" />
+                  </div>
+                </div>
+              </li>
+              <li className="cartItem">
+                <img src="/img/sneakers/sneakers1mini.png" className="cartImg" alt="sneakers Puma X" />
+                <div className="descriptionItem">
+                  <p className="nameSneakers">Кроссовки Puma X Aka Boku Future Rider</p>
+                  <div className="price">
+                    <strong>8 999 руб.</strong>
+                    <img src="/img/deletecart.svg" className="deleteIcon" alt="delete cart" />
+                  </div>
+                </div>
+              </li>
+            </ul>
+            <div className="cartTotal">
+              <div className="total">
+                <span>Итого:</span>
+                <div></div>
+                <strong>21 498 руб.</strong>
+              </div>
+              <div className="charge">
+                <span>Налог 5%:</span>
+                <div></div>
+                <strong>1074 руб.</strong>
+              </div>
+              <a className="placeOrder">
+                Оформить заказ
+                <img src="/img/next.svg" className="orderArrow" alt="next arrow" />
+              </a>
+            </div>
+          </div>
+        </div>
         <header>
           <div className="headerLeft">
             <img height="40" width="40" src="/img/logo.png" className="headerLogo" alt="logo shop" />
@@ -34,7 +77,7 @@ function App() {
           </nav>
       </header>
       <main>
-        <img src="/img/banner.png" className="banners" alt="banner 1" />
+        {/* <img src="/img/banner.png" className="banners" alt="banner 1" /> */}
         <div className="containerTitle">
           <h1>Все кроссовки</h1>
           <div className="searchContainer">
@@ -47,11 +90,11 @@ function App() {
             <li className="card">
               <img src="/img/favorite.svg" className="favoriteIcon" alt="favorite icon" />
               <img src="/img/sneakers/sneakers1.png" className="cardImg" alt="sneakers Puma X" />
-              <h2>Кроссовки Puma X Aka Boku Future Rider</h2>
+              <h2 className="nameSneakers">Кроссовки Puma X Aka Boku Future Rider</h2>
               <div className="addCartContainer">
                 <div className="price">
                   <span>Цена:</span>
-                  <p>8 999 руб.</p>
+                  <strong>8 999 руб.</strong>
                 </div>
                 <img src="/img/addcart.svg" className="addIcon" alt="add cart" />
               </div>
@@ -59,11 +102,11 @@ function App() {
             <li className="card">
               <img src="/img/favorite.svg" className="favoriteIcon" alt="favorite icon" />
               <img src="/img/sneakers/sneakers1.png" className="cardImg" alt="sneakers Puma X" />
-              <h2>Кроссовки Puma X Aka Boku Future Rider</h2>
+              <h2 className="nameSneakers">Кроссовки Puma X Aka Boku Future Rider</h2>
               <div className="addCartContainer">
                 <div className="price">
                   <span>Цена:</span>
-                  <p>8 999 руб.</p>
+                  <strong>8 999 руб.</strong>
                 </div>
                 <img src="/img/addcart.svg" className="addIcon" alt="add cart" />
               </div>
@@ -71,11 +114,11 @@ function App() {
             <li className="card">
               <img src="/img/favorite.svg" className="favoriteIcon" alt="favorite icon" />
               <img src="/img/sneakers/sneakers1.png" className="cardImg" alt="sneakers Puma X" />
-              <h2>Кроссовки Puma X Aka Boku Future Rider</h2>
+              <h2 className="nameSneakers">Кроссовки Puma X Aka Boku Future Rider</h2>
               <div className="addCartContainer">
                 <div className="price">
                   <span>Цена:</span>
-                  <p>8 999 руб.</p>
+                  <strong>8 999 руб.</strong>
                 </div>
                 <img src="/img/addcart.svg" className="addIcon" alt="add cart" />
               </div>
@@ -83,35 +126,11 @@ function App() {
             <li className="card">
               <img src="/img/favorite.svg" className="favoriteIcon" alt="favorite icon" />
               <img src="/img/sneakers/sneakers1.png" className="cardImg" alt="sneakers Puma X" />
-              <h2>Кроссовки Puma X Aka Boku Future Rider</h2>
+              <h2 className="nameSneakers">Кроссовки Puma X Aka Boku Future Rider</h2>
               <div className="addCartContainer">
                 <div className="price">
                   <span>Цена:</span>
-                  <p>8 999 руб.</p>
-                </div>
-                <img src="/img/addcart.svg" className="addIcon" alt="add cart" />
-              </div>
-            </li>
-            <li className="card">
-              <img src="/img/favorite.svg" className="favoriteIcon" alt="favorite icon" />
-              <img src="/img/sneakers/sneakers1.png" className="cardImg" alt="sneakers Puma X" />
-              <h2>Кроссовки Puma X Aka Boku Future Rider</h2>
-              <div className="addCartContainer">
-                <div className="price">
-                  <span>Цена:</span>
-                  <p>8 999 руб.</p>
-                </div>
-                <img src="/img/addcart.svg" className="addIcon" alt="add cart" />
-              </div>
-            </li>
-            <li className="card">
-              <img src="/img/favorite.svg" className="favoriteIcon" alt="favorite icon" />
-              <img src="/img/sneakers/sneakers1.png" className="cardImg" alt="sneakers Puma X" />
-              <h2>Кроссовки Puma X Aka Boku Future Rider</h2>
-              <div className="addCartContainer">
-                <div className="price">
-                  <span>Цена:</span>
-                  <p>8 999 руб.</p>
+                  <strong>8 999 руб.</strong>
                 </div>
                 <img src="/img/addcart.svg" className="addIcon" alt="add cart" />
               </div>
@@ -119,6 +138,7 @@ function App() {
           </ul>
         </div>
       </main>
+    
     </div>
   );
 }
